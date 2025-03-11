a3a405f4fe9f61edf578ba04188a23fd8ad94ca3
Sondos Amr <sondos.amr444@gmail.com>
2025-03-11 16:19:37 +0200

    Added logic to reset current score and visually switch active player when rolling 1. Now, when a player rolls a 1, their current score resets to 0, and the active player class toggles to indicate the turn change

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 const btnReset = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-let diceImg = document.querySelector('.dice');
+const diceImg = document.querySelector('.dice');
 const currentScore0 = document.querySelector('#current--0');
 const currentScore1 = document.querySelector('#current--1');
 const totalScore = document.querySelector('#score--0');
 let finalScores = document.querySelector('.score');
-
+let swithBgColor = document.querySelector('.player--active');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 finalScores.textContent = 0;
 let currentScore = 0;
 let activePlayer = 0;
@@ -24,5 +26,10 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     activePlayer = activePlayer === 0 ? 1 : 0;
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
